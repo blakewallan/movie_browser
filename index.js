73ddb327a6b1cdf39a3f07012dcdcef6c2eafd2f
blakewallan <blakewallan@gmail.com>
2015-12-13 14:47:38 -0800

    Base structure setup

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var db = require('./models');
+var session = require('express-session');
+var ejsLayouts = require('express-ejs-layouts');
+var request = require('request');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+var flash = require('connect-flash');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(flash());
+
+
+
+
+app.listen(process.env.PORT || 3000)
