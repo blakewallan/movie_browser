b3d104af10a572b7d68eb9257a38fdd4de6596fa
blakewallan <blakewallan@gmail.com>
2015-12-14 21:11:02 -0800

    Deleted all auth code and replaced with working code

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 var express = require('express');
-var db = require('./models');
-var session = require('express-session');
-var ejsLayouts = require('express-ejs-layouts');
-var request = require('request');
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt');
+var ejsLayouts = require('express-ejs-layouts');
+var session = require('express-session');
 var flash = require('connect-flash');
+var db = require('./models');
+var passport = require('passport');
+var strategies = require('./config/strategies');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
-app.use(express.static(__dirname + '/static'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
+app.use(session({
+  secret: 'sasdlfkajsldfkajweoriw234234ksdfjals23',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', function(req, res){
-    res.render('index');
+passport.serializeUser(strategies.serializeUser);
+passport.deserializeUser(strategies.deserializeUser);
+
+passport.use(strategies.localStrategy);
+
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
 });
 
+app.use('/', require('./controllers/main'));
+app.use('/auth', require('./controllers/auth'));
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000);
